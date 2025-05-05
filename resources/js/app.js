1b8eb21d6a565c68222ea176b1c33bd4f49a56cd
Sosorioo <Osoriosairon244@gmail.com>
2025-05-05 18:54:58 -0500

    Fix for the cancel button

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,3 @@
 import 'flowbite';
-import Alpine from 'alpinejs'
 /* Encodes current year in the footer */
 document.getElementById("year").textContent = new Date().getFullYear();
-
-window.Alpine = Alpine
-Alpine.start()
